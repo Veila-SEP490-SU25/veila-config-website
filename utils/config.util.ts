265f9998cd06878df74596eff0ee37b563658404
neru <ngocngocthuc@gmail.com>
2025-05-31 16:31:43 +0700

    feat: add API URL configuration and utility function

---
+++ utils/config.util.ts
@@ -0,0 +1,7 @@
+export const getApiURL = (): string => {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  if (!apiUrl) {
+    throw new Error("NEXT_PUBLIC_API_URL is not defined in the environment variables.");
+  }
+  return apiUrl;
+}
